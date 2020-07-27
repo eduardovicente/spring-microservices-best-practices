e55044333b9dbbf54c924f85e1827f415b6595cd
eduardovicente <eduardo.vicente@outlook.es>
2020-07-27 16:00:01 -0500

    Adding common service for users - students

---
+++ microservice-commons-users/src/main/java/com/vicentemartinez/microservices/commons/users/MicroserviceCommonsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.vicentemartinez.microservices.commons.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCommonsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCommonsUsersApplication.class, args);
+	}
+
+}
---
+++ microservice-commons-users/src/test/java/com/vicentemartinez/microservices/commons/users/MicroserviceCommonsUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicentemartinez.microservices.commons.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCommonsUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
